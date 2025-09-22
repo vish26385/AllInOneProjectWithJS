656d0cd7ecdb619c2259960efc32b931c653c582
Vishnu Pandit <vish26385@gmail.com>
2025-09-22 21:00:23 +0530

    Port changes

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
